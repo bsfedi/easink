4a8d90d3934bbcddec34a5af4d3c616aa6abf767
bsfedi <65951975+bsfedi@users.noreply.github.com>
2025-06-12 23:01:53 +0100

    req changes

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 
-from pydantic_settings import BaseSettings
+# from pydantic_settings import BaseSettings
 
-from typing import Optional
+# from typing import Optional
 
-class Settings(BaseSettings):
-    class Config:
-        # `.env.prod` takes priority over `.env`
-        env_file = '.env', '.env.prod'
+# class Settings(BaseSettings):
+#     class Config:
+#         # `.env.prod` takes priority over `.env`
+#         env_file = '.env', '.env.prod'
 
-    BACKEND_URL: str 
-    EMAIL_URL :str
-    BASE_URL: str 
-    PASS_CODE : str
-    EMAIL : str
-    HOST : str
-    DATABASE : str
-    USER : str
-    PASSWORD : str 
-    PORT : str
-    MAIN_PORT : int
+#     BACKEND_URL: str 
+#     EMAIL_URL :str
+#     BASE_URL: str 
+#     PASS_CODE : str
+#     EMAIL : str
+#     HOST : str
+#     DATABASE : str
+#     USER : str
+#     PASSWORD : str 
+#     PORT : str
+#     MAIN_PORT : int
 
 
 
